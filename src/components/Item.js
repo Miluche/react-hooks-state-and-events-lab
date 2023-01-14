517ff4777cf91524655d48e27e3558a79139d761
Collins Kareri <locoreri@gmail.com>
2023-01-14 07:08:53 +0300

    Add and remove from cart logic.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [virtualCart, setVirtualCart] = useState([]);
+
+  function isInCart() {
+    return virtualCart.includes(name);
+  }
+
+  function ToggleItemInCart() {
+    if (isInCart()) {
+      setVirtualCart(virtualCart.filter((cartItems) => cartItems !== name));
+    } else {
+      setVirtualCart([...virtualCart, name]);
+    }
+  }
+
   return (
-    <li className="">
+    <li className={isInCart() ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={ToggleItemInCart}>{isInCart() ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
