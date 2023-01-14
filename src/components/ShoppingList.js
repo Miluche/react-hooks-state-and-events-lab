2d1da95c1dd8d67141c2530774f62782d5b01286
Collins Kareri <locoreri@gmail.com>
2023-01-14 07:09:13 +0300

    Filter items in shopping list logic.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState(items);
+
+  function filterCategory(newFilterCategory) {
+    return items.filter((item) => item.category === newFilterCategory);
+  }
+
+  function selectCategory(evt) {
+    const currentSelectedCategory = evt.target.value;
+
+    if (currentSelectedCategory === "All") {
+      setSelectedCategory([...items]);
+      return;
+    } else {
+      setSelectedCategory(filterCategory(currentSelectedCategory));
+      return;
+    }
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={selectCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {selectedCategory.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
